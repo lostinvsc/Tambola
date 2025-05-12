d0379e802a75322dfb25f960a26fe9a09de82247
sachin <sachinnagar2004@gmail.com>
2025-05-12 18:59:25 +0530

    c5

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 const router = Router();
-const CORRECT_PASSWORD = process.env.PASSWORD || 'admin123'; // You can later move this to an environment variable
+const CORRECT_PASSWORD = process.env.PASSWORD || 'admin123'; 
 
 router.post('/login', (req, res) => {
   const { password } = req.body;
@@ -10,7 +10,7 @@ router.post('/login', (req, res) => {
     return res.status(400).json({ message: 'Password is required' });
   }
 
-  if (password === CORRECT_PASSWORD) {
+  if (password == CORRECT_PASSWORD) {
     return res.status(200).json({ message: 'Login successful', adminToken:"generateTicketS" });
   } else {
     return res.status(401).json({ message: 'Incorrect password' });
