54abb94bd1f5e1a38ecf2d1e3e00da1c1e2edc31
sachin <sachinnagar2004@gmail.com>
2025-05-12 19:25:40 +0530

    lcc

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
